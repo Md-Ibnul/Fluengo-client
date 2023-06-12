67dfa8de35a447807d512d0ba47e19415fc0bb3e
Md-Ibnul <ibnulhasan456@gmail.com>
2023-06-12 18:58:45 +0600

    Payment History added

---
+++ src/Components/Student/EnrollTable.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { deleteSelectClass } from '../../api/select';
+import { toast } from 'react-hot-toast';
+import { FaMoneyCheckAlt, FaUserTie } from 'react-icons/fa';
+import { GiCrossMark } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
+
+const EnrollTable = ({enroll, index}) => {
+    return (
+        <tr>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <div className="ml-3">
+          <p className="text-gray-900 whitespace-no-wrap">{index + 1}</p>
+        </div>
+      </td>
+      <td className=" py-5 border-b border-gray-200 bg-white text-sm">
+        <div className="block relative">
+          <img
+            alt="profile"
+            src={enroll?.classImage}
+            className="mx-auto object-cover rounded h-14 w-14 "
+          />
+        </div>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{enroll?.className}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">
+          {enroll?.instructor.name}
+        </p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap flex items-center">{enroll?.status}
+        </p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap flex items-center">{enroll?.transactionId}
+        </p>
+      </td>
+    </tr>
+    );
+};
+
+export default EnrollTable;
